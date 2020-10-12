8e465b2225a75113baad8330b505fedbd0ef936c
Mai Campbell <jirallc@gmail.com>
2020-10-12 10:39:48 -0400

    style home sceen

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -24,7 +24,7 @@ const Home = () => {
           />
         </div>
       </form>
-      {isLoading ? (
+      {!isLoading ? (
         <div className="loading"> Loading... </div>
       ) : (
         <main className="container">{renderUsers}</main>
